e73a0f99b8e6547ac29e67c2ed2bcb3152db8730
Rodrigo Rezende <rarezende1@gmail.com>
2016-09-05 17:10:52 -0400

    Created GenerateFeatures function

--- GenerateFeatures.py
+++ GenerateFeatures.py
@@ -3,37 +3,48 @@ import scipy.io as sio
 import pandas as pd
 import numpy as np
 
-sourcePath = "C:/Users/Rodrigo/Documents/Data Science/Seizure Prediction/Data/Data1/train_1/"
-destFile = "C:/Users/Rodrigo/Documents/Data Science/Seizure Prediction/Data/train1.csv"
+rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
 
-colNames = ["File", "Class"]
-for i in range(1,33):
-    colNames.append("Feature" + str(i))
+sourceType = "Train"
+sourcePath = rootDir + "Data1/train_1/"
+destFile = rootDir + "train_1.csv"
 
-features = pd.DataFrame(columns = colNames)
+sourceType = "Test"
+sourcePath = rootDir + "Data1/test_1/"
+destFile = rootDir + "test_1.csv"
 
-#fileNames = os.listdir(filePath)
-#for i in range(1,3):
-#    fileName = fileNames[i]
-#    print(fileName)
+def GenerateFeatures(sourcePath, sourceType, destFile):
 
-i = 1
-for fileName in os.listdir(sourcePath):
-    try:
-        fileContents = sio.loadmat(sourcePath + fileName, struct_as_record=False)
-        fileContents = fileContents["dataStruct"]
-        fileContents = fileContents[0,0]
-        eegData = pd.DataFrame(fileContents.data)
-
-        features.loc[i, "File"] = fileName
-        features.loc[i, "Class"] = fileName.split(".")[0][-1]
-        features.loc[i, "Feature1":"Feature16"] = eegData.mean().values
-        features.loc[i, "Feature17":"Feature32"] = eegData.std().values
-        i+=1
+    colNames = ["File"]
+    if sourceType == "Train":
+        colNames.append("Class")
         
-    except ValueError:
-        print("Could not process file: " + fileName)
-        
+    for i in range(1,33):
+        colNames.append("Feature" + str(i))
 
-features.to_csv(destFile)
+    features = pd.DataFrame(columns = colNames)
+
+    i = 1
+    for fileName in os.listdir(sourcePath):
+        try:
+            fileContents = sio.loadmat(sourcePath + fileName, struct_as_record=False)
+            fileContents = fileContents["dataStruct"]
+            fileContents = fileContents[0,0]
+            eegData = pd.DataFrame(fileContents.data)
+
+            features.loc[i, "File"] = fileName
+            if sourceType == "Train":
+                features.loc[i, "Class"] = fileName.split(".")[0][-1]
+            features.loc[i, "Feature1":"Feature16"] = eegData.mean().values
+            features.loc[i, "Feature17":"Feature32"] = eegData.std().values
+            i+=1
+
+        except ValueError:
+            print("Could not process file: " + fileName)
+
+    features.to_csv(destFile)
+
+    return
+
+
     
