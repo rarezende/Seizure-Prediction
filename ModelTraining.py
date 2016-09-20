e4926b773641768cbf8723c0cc7b88c12d07f5e2
Rodrigo Rezende <rarezende1@gmail.com>
2016-09-20 19:00:35 -0400

    Model calibration

--- ModelTraining.py
+++ ModelTraining.py
@@ -47,12 +47,17 @@ submission.to_csv(rootDir + "submission.csv", index = False)
 # -------------------------------------------------------------------------------------- #
 import pandas as pd
 from sklearn import cross_validation
-from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn import svm
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
-rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
-
+#rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
+#numFeatures = 128
+rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/Archive/394 Features/"
 numFeatures = 384
+
 lastFeature = "Feature" + str(numFeatures)
 
 fileName = "train_all.csv"
@@ -60,25 +65,46 @@ trainData = pd.read_csv(rootDir + fileName)
 
 for i in range(1, numFeatures + 1):
     feature = "Feature" + str(i)
-    trainData.loc[trainData[feature].isnull(),feature] = trainData[feature].dropna().median()
-
+    trainData.loc[srcData[feature].isnull(),feature] = srcData[feature].dropna().median()
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(trainData.loc[:, "Feature1":lastFeature].values,
                                                                      trainData.loc[:, "Class"].values, 
                                                                      test_size=0.2, 
                                                                      random_state=31415)
 
-forest = RandomForestClassifier(n_estimators = 30)
-forest = forest.fit(X_train, y_train)
-y_pred = forest.predict(X_test)
 
-cm = confusion_matrix(y_test, y_pred)
+#SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+#    decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+#    max_iter=-1, probability=False, random_state=None, shrinking=True,
+#    tol=0.001, verbose=False)
 
-precision = cm[1,1]/(cm[1,1] + cm[0,1])
-recall = cm[1,1]/(cm[1,1] + cm[1,0])
+#LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, 
+#                   intercept_scaling=1, class_weight=None, random_state=None, solver='liblinear', 
+#                   max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=1
 
-print(cm)
-print("Precision: {} \nRecall: {}".format(precision, recall))
+#classifier = RandomForestClassifier(n_estimators = 30, class_weight= "balanced")
+#classifier = svm.SVC(C=0.0000001, gamma=0.01, class_weight= "balanced")
+classifier = GradientBoostingClassifier(n_estimators= 10)
+
+#classifier = LogisticRegression(C=10, class_weight= "balanced")
+
+X_train = StandardScaler().fit_transform(X_train)
+classifier.fit(X_train, y_train)
+
+
+# Test set predictions
+X_test = StandardScaler().fit_transform(X_test)
+y_pred = classifier.predict(X_test)
+print("Confusion Matrix Test Set")
+print(metrics.confusion_matrix(y_test, y_pred))
+print(metrics.classification_report(y_test, y_pred))
+
+# Training set predictions
+y_pred = classifier.predict(X_train)
+print("----------------------------------------")
+print("Confusion Matrix Train Set")
+print(metrics.confusion_matrix(y_train, y_pred))
+print(metrics.classification_report(y_train, y_pred))
 
 
 
