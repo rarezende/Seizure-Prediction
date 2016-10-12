a78259d5104b8a691e2abcff193fd23734e99067
Rodrigo Rezende <rarezende1@gmail.com>
2016-10-12 13:24:16 -0400

    Minor changes

--- ModelTraining.py
+++ ModelTraining.py
@@ -24,7 +24,7 @@ def param_grid_search(classifier, param_grid):
     print("Best parameters set found:")
     print("----------------------------")
     print("Parameters: " + str(classifier.best_params_))
-    print("ROC AUC Score: {:0.2f}".format(classifier.best_score_))
+    print("ROC AUC Score: {:0.3f}".format(classifier.best_score_))
     print("Total processing time: {:.2f} minutes".format((time.time()-startTime)/60))
     print()
     print("Grid scores:")
@@ -32,7 +32,7 @@ def param_grid_search(classifier, param_grid):
     means = classifier.cv_results_['mean_test_score']
     stds = classifier.cv_results_['std_test_score']
     for mean, std, params in zip(means, stds, classifier.cv_results_['params']):
-        print("{:0.2f} (+/-{:0.2f}) for {}".format(mean, std, params))
+        print("{:0.3f} (+/-{:0.3f}) for {}".format(mean, std, params))
 
 
 
@@ -46,13 +46,13 @@ def param_grid_search(classifier, param_grid):
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression()
 param_grid = [
-    {'C':[0.03, 0.1, 0.3, 1, 3, 10, 30, 100], 
-     'fit_intercept': [True, False], 
-     'class_weight': [None, 'balanced', {0:0.05, 1:0.95}, {0:0.95, 1:0.05}]},
+    {'C':[0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3], 
+     'class_weight': ['balanced', {0:0.01, 1:0.99}, {0:0.005, 1:0.995}, {0:0.001, 1:0.999}]},
 ]
 
-# Parameters: {'fit_intercept': True, 'class_weight': {0: 0.05, 1: 0.95}, 'C': 0.3}
-# Score: 0.62
+# Parameters: {'C': 0.003, 'class_weight': {0: 0.001, 1: 0.999}}
+# ROC AUC Score: 0.66
+# Total processing time: 1.45 minutes
 
 
 # -------------------------------------------------------------------------------------- #
@@ -125,8 +125,8 @@ X = trainData.loc[:, "Feature1":].values
 y = trainData.loc[:, "Class"].values
 
 # -------- Change model here --------------
-from sklearn.ensemble import GradientBoostingClassifier
-classifier = GradientBoostingClassifier(n_estimators = 3000, max_depth= 3, learning_rate = 0.01, min_samples_split= 5)
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(C=0.03, class_weight = {0: 0.01, 1: 0.99})
 
 classifier = make_pipeline(StandardScaler(), classifier)
 scores = cross_val_score(classifier, X, y, cv=5, scoring = "roc_auc", n_jobs=-1)
@@ -150,8 +150,8 @@ y_train = trainData.loc[:, "Class"].values
 X_test  = testData.loc[:, "Feature1":].values
 
 # -------- Change model here --------------
-from sklearn.ensemble import GradientBoostingClassifier
-classifier = GradientBoostingClassifier(n_estimators = 3000, max_depth= 3, learning_rate = 0.01, min_samples_split= 5)
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(C=0.03, class_weight = {0: 0.01, 1: 0.99})
 
 X_train = StandardScaler().fit_transform(X_train)
 classifier.fit(X_train, y_train)
@@ -162,5 +162,27 @@ y_pred = classifier.predict(X_test)
 submission = pd.DataFrame(testData["File"])
 submission["Class"] = y_pred
 submission.to_csv(rootDir + "submission.csv", index = False)
+
+
+
+# -------------------------------------------------------------------------------------- #
+# Load epochData for testing
+# -------------------------------------------------------------------------------------- #
+import os
+import scipy.io as sio
+
+rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
+sourcePath = rootDir + "Data1/train_1/"
+fileNames = os.listdir(sourcePath)
+fileName = fileNames[0]
+fileContents = sio.loadmat(sourcePath + fileName, struct_as_record=False)
+fileContents = fileContents["dataStruct"][0,0]
+eegData = fileContents.data
+timeWindows = [24000*time for time in range(11)] # 1 minute clips
+i=0
+channel = 0
+epochData = eegData[timeWindows[i]:timeWindows[i+1],channel]
+
+
 
 
