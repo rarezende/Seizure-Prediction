cd9519d2fae5e30e85d124c5f7609f73813f8e9e
Rodrigo Rezende <rarezende1@gmail.com>
2016-11-03 15:26:15 -0400

    Neural Networks

--- ModelTraining.py
+++ ModelTraining.py
@@ -43,10 +43,13 @@ import numpy
 import time
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.utils import np_utils
+from keras.layers import Convolution2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 numpy.random.seed(2016)
 
@@ -59,34 +62,172 @@ trainData = pd.read_csv(rootDir + fileName)
 X = trainData.loc[:, "Feature1":].values
 y = trainData.loc[:, "Class"].values
 
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2016)
+
+#X = X_train
+#y = y_train
+
 X = StandardScaler().fit_transform(X)
 
-numFeatures = X.shape[1]
+# process the data to fit in a keras CNN properly
+# input data needs to be (N, C, X, Y) - shaped where
+# N - number of samples
+# C - number of channels per sample
+# (X, Y) - sample size
 
-# create model
+X = X.reshape((X.shape[0], 1, X.shape[1], 1))
+
+# output labels should be one-hot vectors - ie,
+# 0 -> [0, 0, 1]
+# 1 -> [0, 1, 0]
+# 2 -> [1, 0, 0]
+# this operation changes the shape of y from (10000,1) to (10000, 3)
+
+y = np_utils.to_categorical(y)
+
 def create_model():
-    numFeatures = 1560
-    model = Sequential()
-    model.add(Dense(numFeatures, input_dim=numFeatures, init='uniform', activation='relu'))
-    model.add(Dense(numFeatures, init='uniform', activation='relu'))
-    model.add(Dense(numFeatures, init='uniform', activation='relu'))
-    model.add(Dense(1, init='uniform', activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+    cnn = Sequential()
+    cnn.add(Convolution2D(64, 3, 1, border_mode="same", activation="relu", input_shape=(1, 1560, 1)))
+    #cnn.add(Convolution2D(64, 3, 1, border_mode="same", activation="relu"))
+    cnn.add(MaxPooling2D(pool_size=(2,1)))
 
-'''
-# Fit the model
-model.fit(X, y, nb_epoch=5, batch_size=100)
-'''
+    cnn.add(Convolution2D(128, 3, 1, border_mode="same", activation="relu"))
+    #cnn.add(Convolution2D(128, 3, 1, border_mode="same", activation="relu"))
+    #cnn.add(Convolution2D(128, 3, 1, border_mode="same", activation="relu"))
+    cnn.add(MaxPooling2D(pool_size=(2,1)))
 
-classifier = KerasClassifier(build_fn = create_model, nb_epoch=100, batch_size=250, verbose=0)
+    cnn.add(Convolution2D(256, 3, 1, border_mode="same", activation="relu"))
+    #cnn.add(Convolution2D(256, 3, 1, border_mode="same", activation="relu"))
+    #cnn.add(Convolution2D(256, 3, 1, border_mode="same", activation="relu"))
+    cnn.add(MaxPooling2D(pool_size=(2,1)))
+
+    cnn.add(Flatten())
+    cnn.add(Dense(1024, activation="relu"))
+    cnn.add(Dropout(0.5))
+    cnn.add(Dense(2, activation="softmax"))
+
+    cnn.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+    
+    return cnn
+
+
+classifier = KerasClassifier(build_fn = create_model, nb_epoch=1, batch_size=250, verbose=True)
 
 #classifier.fit(X, y)
 #predictions = classifier.predict_proba(X)
 
 scores = cross_val_score(classifier, X, y, cv=5, scoring = "roc_auc")
 print("ROC AUC: {:.3f} (+/- {:.3f})".format(scores.mean(), scores.std()))
+
 print("Total processing time: {:.2f} minutes".format((time.time()-startTime)/60))
+
+
+import numpy
+import time
+import pandas as pd
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers import Convolution1D, Dense, Dropout, Flatten, MaxPooling1D
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+numpy.random.seed(2016)
+
+startTime = time.time()
+
+rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
+fileName = "train_all.csv"
+trainData = pd.read_csv(rootDir + fileName)
+
+X = trainData.loc[:, "Feature1":].values
+y = trainData.loc[:, "Class"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2016)
+
+X_train = StandardScaler().fit_transform(X_train)
+X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
+
+X_test = StandardScaler().fit_transform(X_test)
+X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+cnn = Sequential()
+cnn.add(Convolution1D(10, 100, border_mode="same", activation="relu", input_shape = (1, 1560)))
+cnn.add(Flatten())
+cnn.add(Dense(128, activation="relu"))
+cnn.add(Dropout(0.5))
+cnn.add(Dense(2, activation="sigmoid"))
+cnn.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+cnn.fit(X_train, y_train, nb_epoch=5, batch_size=100)
+
+y_pred = cnn.predict_proba(X_test)
+
+print(roc_auc_score(y_test[:,1], y_pred[:,1]))
+
+print("Total processing time: {:.2f} minutes".format((time.time()-startTime)/60))
+
+
+
+
+import time
+import pandas as pd
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers import Convolution1D, Dense, Dropout, Flatten, MaxPooling1D
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+
+startTime = time.time()
+
+rootDir = "C:/Users/Rodrigo/Documents/Data Science/Seizure-Prediction/Data/"
+fileName = "train_all.csv"
+trainData = pd.read_csv(rootDir + fileName)
+
+X = trainData.loc[:, "Feature1":].values
+y = trainData.loc[:, "Class"].values
+
+X = StandardScaler().fit_transform(X)
+X = X.reshape((X.shape[0], 1, X.shape[1]))
+
+y = np_utils.to_categorical(y)
+
+def create_model():
+    cnn = Sequential()
+    cnn.add(Convolution1D(32, 128, border_mode="same", activation="relu", input_shape = (1, 1560)))
+    cnn.add(Convolution1D(32, 128, border_mode="same", activation="relu"))
+    cnn.add(Convolution1D(32, 128, border_mode="same", activation="relu"))
+    cnn.add(Convolution1D(32, 128, border_mode="same", activation="relu"))
+    cnn.add(Flatten())
+    cnn.add(Dense(2048, activation="relu"))
+    cnn.add(Dropout(0.5))
+    cnn.add(Dense(1024, activation="relu"))
+    cnn.add(Dropout(0.5))
+    cnn.add(Dense(512, activation="relu"))
+    cnn.add(Dropout(0.5))
+    cnn.add(Dense(2, activation="sigmoid"))
+    cnn.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['fbeta_score'])
+
+    return cnn
+
+classifier = KerasClassifier(build_fn = create_model, nb_epoch=3, batch_size=100, verbose=True)
+
+scores = cross_val_score(classifier, X, y, cv=5, scoring = "roc_auc")
+print("")
+print("ROC AUC: {:.3f} (+/- {:.3f})".format(scores.mean(), scores.std()))
+
+print("Total processing time: {:.2f} minutes".format((time.time()-startTime)/60))
+
+
+
+
 
 
 
@@ -173,6 +314,9 @@ print("ROC AUC: {:.3f} (+/- {:.3f})".format(scores.mean(), scores.std()))
 # -------------------------------------------------------------------------------------- #
 # Create submission file
 # -------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------- #
+# Create submission file
+# -------------------------------------------------------------------------------------- #
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
@@ -183,18 +327,16 @@ testData = pd.read_csv(rootDir + "test_all.csv")
 X_train = trainData.loc[:, "Feature1":].values
 y_train = trainData.loc[:, "Class"].values
 
-X_test  = testData.loc[:, "Feature1":].values
-
-# -------- Change model here --------------
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(C=0.001, class_weight = {0: 0.001, 1: 0.999})
-#from sklearn.ensemble import RandomForestClassifier
-#classifier = RandomForestClassifier(n_estimators = 3000, class_weight = 'balanced')
-
 X_train = StandardScaler().fit_transform(X_train)
+X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
+y_train = np_utils.to_categorical(y_train)
+
+X_test  = testData.loc[:, "Feature1":].values
+X_test = StandardScaler().fit_transform(X_test)
+X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+
 classifier.fit(X_train, y_train)
 
-X_test = StandardScaler().fit_transform(X_test)
 y_pred = classifier.predict_proba(X_test)
 
 submission = pd.DataFrame(testData["File"])
@@ -268,5 +410,18 @@ channel = 0
 epochData = eegData[timeWindows[i]:timeWindows[i+1],channel]
 
 
+# -------------------------------------------------------------------------------------- #
+# Reshape
+# -------------------------------------------------------------------------------------- #
+import numpy as np
+x = np.arange(10)
+x = np.concatenate([x,x,x,x])
+print(x.shape)
+
+y = np.vstack([x,x,x])
+print(y.shape)
+
+z = y.reshape(3,4,10)
+print(z)
 
 
